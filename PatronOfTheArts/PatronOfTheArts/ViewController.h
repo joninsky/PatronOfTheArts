90384c3b6cdbb7efbf1f5289b77f1e84621d8a92
Jon Vogel <joninsky@gmail.com>
2015-02-20 16:31:01 -0800

    All Files

---
+++ PatronOfTheArts/PatronOfTheArts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PatronOfTheArts
+//
+//  Created by Jon Vogel on 2/20/15.
+//  Copyright (c) 2015 Jon Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PatronOfTheArts/PatronOfTheArts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PatronOfTheArts
+//
+//  Created by Jon Vogel on 2/20/15.
+//  Copyright (c) 2015 Jon Vogel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
